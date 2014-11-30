a70be84a2d48d00d297d603a38daa3118fa937c8
lykarik <9yards.to.nowhere@gmail.com>
2014-11-30 21:12:45 +0500

    lab 1

---
+++ first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+	
+int main()
+{
+	printf("This is to demonstrate fork() PID=%d\n",(int) getpid());
+	fork();
+	printf("Hello world PID=%d\n", (int) getpid());
+
+return 0;
+}
