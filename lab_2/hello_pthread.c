3b591fe2e49f2edf6484cc828513b0a44a027b51
lykarik <9yards.to.nowhere@gmail.com>
2014-12-19 18:26:51 +0500

    update

--- lab_2/hello_pthread.c
+++ lab_2/hello_pthread.c
@@ -1,11 +1,14 @@
-//gcc Hello_pthread.c -lpthread
+/*Hello, world with use pthread
+	for compilating use key -lpthread
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h> 
 #include <pthread.h>
 
-void *thread(void *vargp) {  //функция для нити
+void *thread(void *vargp) { 
 	printf("hello world\n");
 	pthread_exit((void*)1);
 }
@@ -13,19 +16,24 @@ void *thread(void *vargp) {  //функция для нити
 int main(int argc, char** argv) {
 	if (argc == 2) {
 		int count = atoi(argv[1]);
-		pthread_t* array = malloc(count * sizeof(pthread_t)); //массив индексов, в котором храним инфу о нитях
+		pthread_t* array = malloc(count * sizeof(pthread_t)); 
 		int i;
+		
 		for (i = 0; i < count; i++) {
-			if (pthread_create(&array[i], NULL, thread, NULL) != 0) { //создаём нити
+			if (pthread_create(&array[i], NULL, thread, NULL) != 0) { 
 				perror("Cannot create thread");
 			}
 		}
+		
 		int ans;
-		for (i = 0; i < count; i++) { //собираем нити обратно
+		
+		for (i = 0; i < count; i++) { 
 			pthread_join(array[i], NULL);
 		}
+		
 		printf("goodbye world\n");
 		free(array);
+	
 	} else {
 		perror("Wrong Count of Arguments");
 		return -1;
